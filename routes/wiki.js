d70f00ac53ecedfb6e3b7b37e1ccdf2b2566a3ea
Alona Varshal <thirdwave72@gmail.com>
2018-03-09 14:08:06 -0600

    Adding slug and styling slug page

--- routes/wiki.js
+++ routes/wiki.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router();
 const { Page } = require('../models')
 const addPage = require('../views/addPage');
+const wikiPage = require('../views/wikipage')
 
 
 router.get('/', (req, res, next) => {
@@ -27,7 +28,7 @@ router.post('/', async (req, res, next) => {
   try {
     await page.save();
     console.log(page);
-    res.redirect('/');
+    res.redirect(`/wiki/${page.slug}`);
   } catch (error) { next(error) }
 
 });
@@ -36,5 +37,15 @@ router.get('/add', (req, res, next) => {
     res.send(addPage());
 });
 
+router.get('/:slug', async (req, res, next) => {
+  try {
+    const page = await Page.findOne({
+      where: {
+        slug: req.params.slug
+      }
+    });
+    res.send(wikiPage(page));
+  } catch (error) { next(error) }
+});
 
 module.exports = router;

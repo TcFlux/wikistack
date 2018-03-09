c6d9737ef118a023107084690145727a202aba1b
Tyler Chan <TcFlux@gmail.com>
2018-03-09 12:39:42 -0500

    fix model export to app.js

--- app.js
+++ app.js
@@ -2,12 +2,16 @@ const express = require('express')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
 const path = require('path')
-
+const models = require('./models')
 const app = express()
 
 app.use(morgan('dev'))
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')))
+
+// db.authenticate().then(() => {
+//     console.log("Connected to database!");
+// })
 
 app.get('/', (req, res) => {
     res.send("Homepage!");
@@ -15,6 +19,13 @@ app.get('/', (req, res) => {
 
 const PORT = 3000;
 
-app.listen(PORT, () => {
-    console.log(`Listening at ${PORT}`)
-});
+
+const init = async () => {
+    await models.db.sync({force: true});
+
+    app.listen(PORT, () => {
+        console.log(`Listening at ${PORT}`)
+    });
+}
+
+init();
